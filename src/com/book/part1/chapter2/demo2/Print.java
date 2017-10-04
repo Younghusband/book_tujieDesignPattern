b842ccee448ad9ec8b70abed74d683aa2cc6d20c
vermouth.yf <vermouth.yf@gmail.com>
2017-10-04 20:53:35 +0800

    restart at 10.4

--- src/com/book/part1/chapter2/demo2/Print.java
+++ src/com/book/part1/chapter2/demo2/Print.java
@@ -7,6 +7,7 @@ package com.book.part1.chapter2.demo2;
     * @date ：2017年2月16日 下午5:57:17 
 **************************************************/
 public abstract class Print {
+//  public interface Print {
      public abstract void printStrong();
      public abstract void printWeak();
 }
--- src/com/book/part1/chapter2/demo2/PrintBanner.java
+++ src/com/book/part1/chapter2/demo2/PrintBanner.java
@@ -6,8 +6,9 @@ package com.book.part1.chapter2.demo2;
     * @date ：2017年2月16日 下午5:58:38 
 **************************************************/
 public class PrintBanner extends Print {
+//  public class PrintBanner implements Print{   //并没有看出来这里用抽象类好在哪
+	  
     private Banner banner;
-	
 	public PrintBanner(String s){
 		banner = new Banner(s);
 	}
